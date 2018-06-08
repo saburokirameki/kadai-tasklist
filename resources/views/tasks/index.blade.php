91ecae29e113e4b385e3abcc32610ecc506e5a39
aki <niconico@gmail.com>
2018-06-08 04:25:17 +0000

    finishtask

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,11 +16,13 @@
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
+                 @if (Auth::user()->id == $task->user_id)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                         <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
+                    @endif
                 @endforeach
             </tbody>
         </table>
